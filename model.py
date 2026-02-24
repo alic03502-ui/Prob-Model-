59ab15e27ff939e5a8d8f4dc98e5b6f8aac5437d
alic03502-ui <alic03502@gmail.com>
2026-02-25 02:28:56 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,148 +1,112 @@
-import numpy as np
+import math
 from scipy.stats import poisson
 
+MAX_GOALS = 10
+
 def fair_odds(prob):
-    if prob == 0:
-        return 0
-    return round(1 / prob, 2)
+    return 1 / prob if prob > 0 else 0
 
-def calculate_match(home_xg_home, home_xga_home,
-                    away_xg_away, away_xga_away,
-                    league_avg):
+def score_matrix(home_xg, away_xg):
+    matrix = []
+    for i in range(MAX_GOALS):
+        row = []
+        for j in range(MAX_GOALS):
+            row.append(poisson.pmf(i, home_xg) * poisson.pmf(j, away_xg))
+        matrix.append(row)
+    return matrix
 
-    # --- Expected goals ---
-    home_xg = (home_xg_home * away_xga_away) / league_avg
-    away_xg = (away_xg_away * home_xga_home) / league_avg
-    total_xg = home_xg + away_xg
+def match_outcomes(matrix):
+    home = draw = away = 0
+    for i in range(MAX_GOALS):
+        for j in range(MAX_GOALS):
+            if i > j:
+                home += matrix[i][j]
+            elif i == j:
+                draw += matrix[i][j]
+            else:
+                away += matrix[i][j]
+    return home, draw, away
 
-    max_goals = 10
+def total_prob(matrix, line):
+    under = 0
+    for i in range(MAX_GOALS):
+        for j in range(MAX_GOALS):
+            if i + j <= line:
+                under += matrix[i][j]
+    over = 1 - under
+    return over, under
 
-    home_probs = [poisson.pmf(i, home_xg) for i in range(max_goals)]
-    away_probs = [poisson.pmf(i, away_xg) for i in range(max_goals)]
-    matrix = np.outer(home_probs, away_probs)
+def team_total_prob(xg, line):
+    under = poisson.cdf(line, xg)
+    over = 1 - under
+    return over, under
 
-    # --- Goal markets ---
-    def prob_total(threshold, over=True):
-        if over:
-            return sum(matrix[i][j] for i in range(max_goals)
-                                      for j in range(max_goals)
-                                      if i + j >= threshold)
-        else:
-            return sum(matrix[i][j] for i in range(max_goals)
-                                      for j in range(max_goals)
-                                      if i + j <= threshold)
+def btts_prob(matrix):
+    yes = 0
+    for i in range(1, MAX_GOALS):
+        for j in range(1, MAX_GOALS):
+            yes += matrix[i][j]
+    no = 1 - yes
+    return yes, no
 
-    over_0_5 = prob_total(1)
-    over_1_5 = prob_total(2)
-    over_2_5 = prob_total(3)
-    over_3_5 = prob_total(4)
-    over_4_5 = prob_total(5)
+def calculate_all_markets(home_xg, away_xg, league_avg):
 
-    under_0_5 = 1 - over_0_5
-    under_1_5 = 1 - over_1_5
-    under_2_5 = 1 - over_2_5
-    under_3_5 = 1 - over_3_5
-    under_4_5 = 1 - over_4_5
+    matrix = score_matrix(home_xg, away_xg)
+    home_win, draw, away_win = match_outcomes(matrix)
 
-    # --- BTTS ---
-    btts = sum(matrix[i][j] for i in range(1, max_goals)
-                              for j in range(1, max_goals))
+    results = {}
 
-    # --- Match result ---
-    home_win = sum(matrix[i][j] for i in range(max_goals)
-                                  for j in range(max_goals)
-                                  if i > j)
+    # 1X2
+    results["Home Win"] = (home_win, fair_odds(home_win))
+    results["Draw"] = (draw, fair_odds(draw))
+    results["Away Win"] = (away_win, fair_odds(away_win))
 
-    draw = sum(matrix[i][j] for i in range(max_goals)
-                              for j in range(max_goals)
-                              if i == j)
+    # Double chance
+    results["1X"] = (home_win + draw, fair_odds(home_win + draw))
+    results["X2"] = (away_win + draw, fair_odds(away_win + draw))
+    results["12"] = (home_win + away_win, fair_odds(home_win + away_win))
 
-    away_win = sum(matrix[i][j] for i in range(max_goals)
-                                  for j in range(max_goals)
-                                  if i < j)
+    # Totals
+    for line in [0.5, 1.5, 2.5, 3.5, 4.5]:
+        over, under = total_prob(matrix, int(line))
+        results[f"Over {line}"] = (over, fair_odds(over))
+        results[f"Under {line}"] = (under, fair_odds(under))
 
-    one_x = home_win + draw
-    x_two = away_win + draw
-    one_two = home_win + away_win
+    # BTTS
+    btts_yes, btts_no = btts_prob(matrix)
+    results["BTTS Yes"] = (btts_yes, fair_odds(btts_yes))
+    results["BTTS No"] = (btts_no, fair_odds(btts_no))
 
-    # --- Team totals ---
-    def team_total(probs, threshold):
-        return sum(probs[i] for i in range(threshold, max_goals))
+    # Team totals
+    for line in [0.5, 1.5, 2.5]:
+        over_h, under_h = team_total_prob(home_xg, int(line))
+        over_a, under_a = team_total_prob(away_xg, int(line))
 
-    home_over_0_5 = team_total(home_probs, 1)
-    home_over_1_5 = team_total(home_probs, 2)
-    home_over_2_5 = team_total(home_probs, 3)
+        results[f"Home Over {line}"] = (over_h, fair_odds(over_h))
+        results[f"Home Under {line}"] = (under_h, fair_odds(under_h))
+        results[f"Away Over {line}"] = (over_a, fair_odds(over_a))
+        results[f"Away Under {line}"] = (under_a, fair_odds(under_a))
 
-    away_over_0_5 = team_total(away_probs, 1)
-    away_over_1_5 = team_total(away_probs, 2)
-    away_over_2_5 = team_total(away_probs, 3)
+    # Combos with totals
+    over15, _ = total_prob(matrix, 1)
+    over25, _ = total_prob(matrix, 2)
 
-    # --- Combo markets ---
-    def combo(condition):
-        return sum(matrix[i][j] for i in range(max_goals)
-                                  for j in range(max_goals)
-                                  if condition(i, j))
+    results["1X + Over 1.5"] = ((home_win + draw) * over15,
+                               fair_odds((home_win + draw) * over15))
 
-    one_x_o15 = combo(lambda i, j: (i >= j) and (i + j >= 2))
-    one_x_o25 = combo(lambda i, j: (i >= j) and (i + j >= 3))
-    one_x_u35 = combo(lambda i, j: (i >= j) and (i + j <= 3))
+    results["1X + Over 2.5"] = ((home_win + draw) * over25,
+                               fair_odds((home_win + draw) * over25))
 
-    x_two_o15 = combo(lambda i, j: (i <= j) and (i + j >= 2))
-    x_two_o25 = combo(lambda i, j: (i <= j) and (i + j >= 3))
-    x_two_u35 = combo(lambda i, j: (i <= j) and (i + j <= 3))
+    results["X2 + Over 1.5"] = ((away_win + draw) * over15,
+                               fair_odds((away_win + draw) * over15))
 
-    markets = {
-        "Over 0.5": over_0_5,
-        "Over 1.5": over_1_5,
-        "Over 2.5": over_2_5,
-        "Over 3.5": over_3_5,
-        "Over 4.5": over_4_5,
+    results["X2 + Over 2.5"] = ((away_win + draw) * over25,
+                               fair_odds((away_win + draw) * over25))
 
-        "Under 0.5": under_0_5,
-        "Under 1.5": under_1_5,
-        "Under 2.5": under_2_5,
-        "Under 3.5": under_3_5,
-        "Under 4.5": under_4_5,
+    results["Home Win + Over 1.5"] = (home_win * over15,
+                                     fair_odds(home_win * over15))
 
-        "BTTS": btts,
+    results["Away Win + Over 1.5"] = (away_win * over15,
+                                     fair_odds(away_win * over15))
 
-        "Home Win": home_win,
-        "Draw": draw,
-        "Away Win": away_win,
-        "1X": one_x,
-        "X2": x_two,
-        "12": one_two,
-
-        "Home O0.5": home_over_0_5,
-        "Home O1.5": home_over_1_5,
-        "Home O2.5": home_over_2_5,
-
-        "Away O0.5": away_over_0_5,
-        "Away O1.5": away_over_1_5,
-        "Away O2.5": away_over_2_5,
-
-        "1X + O1.5": one_x_o15,
-        "1X + O2.5": one_x_o25,
-        "1X + U3.5": one_x_u35,
-
-        "X2 + O1.5": x_two_o15,
-        "X2 + O2.5": x_two_o25,
-        "X2 + U3.5": x_two_u35,
-    }
-
-    # --- Convert to % and fair odds ---
-    output = {
-        "home_xg": round(home_xg, 2),
-        "away_xg": round(away_xg, 2),
-        "total_xg": round(total_xg, 2),
-        "markets": {}
-    }
-
-    for market, prob in markets.items():
-        prob = round(prob, 4)
-        output["markets"][market] = {
-            "probability_%": round(prob * 100, 2),
-            "fair_odds": fair_odds(prob)
-        }
-
-    return output
+    return results
